2984bdb629c40b374a6ed9d35705e33db6b25de5
Francesco Infante <francesco@linkurio.us>
2017-01-16 13:20:52 +0100

    Update main_static.js

--- template/main_static.js
+++ template/main_static.js
@@ -1,7 +1,7 @@
 // Bootstrap Scrollspy
 $(this).scrollspy({ target: '#scrollingNav', offset: 18 });
 
-// Content-Scroll on Navigation click.
+// Content-Scroll on Navigation click
 $('.sidenav').find('a').on('click', function(e) {
   e.preventDefault();
   var id = $(this).attr('href');
@@ -10,7 +10,7 @@ $('.sidenav').find('a').on('click', function(e) {
   window.location.hash = $(this).attr('href');
 });
 
-// Quickjump on Pageload to hash position.
+// Quickjump on Pageload to hash position
 if(window.location.hash) {
   var id = window.location.hash;
   if ($(id).length > 0)
@@ -23,3 +23,9 @@ $('a[data-toggle=popover]')
   .click(function(e) {
     e.preventDefault();
   });
+
+
+$('.nav-tabs-examples a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
